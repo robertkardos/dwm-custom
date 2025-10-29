d4863057511b701f6bf8bba9961cf297d06ffe34
Robert Kardos <kardos.robert.kh@gmail.com>
2025-10-29 01:28:06 +0100

    Add handling for Rofi window selector

--- config.h
+++ config.h
@@ -63,6 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
+static const char *rofiwindowselect[]  = { "rofi", "-show", "window", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -100,6 +101,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,                       XK_r,      togglepreviewallwin,  {0} },
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = rofiwindowselect} },
 };
 
 /* button definitions */
